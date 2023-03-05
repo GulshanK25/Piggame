699ef6c4b8717d7de0a347875c8294f6d158a646
GulshannK <gulshan.kumar0003@stud.hkr.se>
2023-03-05 11:49:53 +0100

    Doc string added

--- Player.py
+++ Player.py
@@ -1,6 +1,27 @@
+# pylint: disable=import-error
+import sys
+
+
 from Dice import Die
 
+
 class Player:
+    """
+    A class representing a player in the Pig Dice game.
+
+    Attributes:
+    ----------
+    name : str
+        The name of the player.
+    roundscore : int
+        The score obtained by the player in the current round.
+    playerscore : int
+        The total score obtained by the player in the game.
+    Methods:
+    -------
+    round()
+        Simulates a round of the Pig Dice game for the player.
+    """
     def __init__(self, name):
         self.name = name
         self.roundscore = 0
@@ -10,33 +31,41 @@ class Player:
         return f"{self.name}"
 
     def round(self):
+        """
+        Simulates a round of the Pig Dice game for the player.
+
+        The player has the option to roll the die or hold at each turn.
+        If the player rolls a 1, the round ends and the player loses
+        all the points obtained in that round. If the player holds,
+        the points obtained in the round are added to the player's total
+        score. If the player quits, the game ends.
+        """
         choice = "r"
         while choice != 'h':
             choice = input("Do you want to (r)oll or (h)old? or (q)uit? ")
-            #  giving options to the player to roll or hold or quit the game
-            if choice == "cheatwin":  # cheat code for the game
+            if choice == "cheatwin":
                 self.roundscore = 100
                 break
-            if choice != 'r' and choice != 'h' and choice != 'q':
+            if choice not in {'r', 'h', 'q'}:
                 print("Invalid choice. Try again please.")
                 continue
             if choice == 'h':
                 break
-            elif choice == 'r':
+            if choice == 'r':
                 roll = Die().value
                 print(f"You rolled {roll}")
                 if roll != 1:
                     self.roundscore += roll
                     print(f"{self.name}'s score for this round is"
                           f" {self.roundscore}")
-                    # printing out the score
                 else:
                     print("Sorry You Pigged!!!")
                     self.roundscore = 0
                     break
             else:
                 print("You quit the game.")
-                exit()
+                sys.exit()
         self.playerscore += self.roundscore
         print(f"Total score for {self.name} is {self.playerscore}")
         self.roundscore = 0
+
